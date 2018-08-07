be0607e35b88964ece8a9455f26d143b1e516f30
yyl <yuanyu.lin@gmail.com>
2018-08-07 16:20:25 +0800

    MAJOR_NUM=1 MINOR_NUM=2 AUX_NUM=0 update package

--- Package/CONFIG.py
+++ Package/CONFIG.py
@@ -32,6 +32,7 @@ def set_global(args):
     global dst_usr_local_libexec_dir
     global dst_usr_local_share_dir
     global dst_pkgconfig_dir
+    global install_test_utils
     pkg_path = args["pkg_path"]
     output_dir = args["output_path"]
     tarball_pkg = ops.path_join(pkg_path, TARBALL_FILE)
@@ -48,6 +49,10 @@ def set_global(args):
     dst_usr_local_libexec_dir = ops.path_join(install_dir, "usr/local/libexec")
     dst_usr_local_share_dir = ops.path_join(install_dir, "usr/local/share")
     dst_pkgconfig_dir = ops.path_join(ops.path_join(output_dir, "pkgconfig"), "pkgconfig")
+    if ops.getEnv("INSTALL_TEST_UTILS") == 'y':
+        install_test_utils = True
+    else:
+        install_test_utils = False
 
 def MAIN_ENV(args):
     set_global(args)
@@ -56,8 +61,8 @@ def MAIN_ENV(args):
     ops.exportEnv(ops.setEnv("CXX", ops.getEnv("CROSS_COMPILE") + "g++"))
     ops.exportEnv(ops.setEnv("CROSS", ops.getEnv("CROSS_COMPILE")))
     ops.exportEnv(ops.setEnv("DESTDIR", install_tmp_dir))
-    ops.exportEnv(ops.setEnv("PKG_CONFIG_LIBDIR", ops.path_join(iopc.getSdkPath(), "pkgconfig")))
-    ops.exportEnv(ops.setEnv("PKG_CONFIG_SYSROOT_DIR", iopc.getSdkPath()))
+    #ops.exportEnv(ops.setEnv("PKG_CONFIG_LIBDIR", ops.path_join(iopc.getSdkPath(), "pkgconfig")))
+    #ops.exportEnv(ops.setEnv("PKG_CONFIG_SYSROOT_DIR", iopc.getSdkPath()))
 
     cc_sysroot = ops.getEnv("CC_SYSROOT")
     cflags = ""
@@ -78,9 +83,9 @@ def MAIN_ENV(args):
     libs = ""
     libs += " -lffi -lxml2 -lexpat -lxkbcommon -levdev -ludev -lmtdev -lpixman-1 -lpng -lz -lcairo -ldrm -lgbm -lwayland-server"
     libs += " -lEGL -ludev -lgbm -ldrm -lexpat -lwayland-client -lglapi -lffi -lwayland-server -lGLESv2"
-    ops.exportEnv(ops.setEnv("LDFLAGS", ldflags))
-    ops.exportEnv(ops.setEnv("CFLAGS", cflags))
-    ops.exportEnv(ops.setEnv("LIBS", libs))
+    #ops.exportEnv(ops.setEnv("LDFLAGS", ldflags))
+    #ops.exportEnv(ops.setEnv("CFLAGS", cflags))
+    #ops.exportEnv(ops.setEnv("LIBS", libs))
 
     return False
 
@@ -129,46 +134,70 @@ def MAIN_CONFIGURE(args):
     extra_conf.append("--disable-setuid-install")
     extra_conf.append("--enable-egl")
     extra_conf.append("--disable-wcap-tools")
-    extra_conf.append("--disable-simple-egl-clients")
-    extra_conf.append("--disable-demo-clients-install")
+    extra_conf.append("--enable-simple-egl-clients")
+    extra_conf.append("--enable-demo-clients-install")
     #extra_conf.append("--with-host-scanner")
     #extra_conf.append("--disable-documentation")
-    extra_conf.append('LIBDRM_CFLAGS=-I' + ops.path_join(iopc.getSdkPath(), 'usr/include/libdrm') + ' -I' + ops.path_join(iopc.getSdkPath(), 'usr/include/libdrm/libdrm'))
-    extra_conf.append('LIBDRM_LIBS=-L' + ops.path_join(iopc.getSdkPath(), 'lib') + ' -ldrm')
-    extra_conf.append('DRM_COMPOSITOR_CFLAGS=-I' + ops.path_join(iopc.getSdkPath(), 'usr/include/libdrm') + ' -I' + ops.path_join(iopc.getSdkPath(), 'usr/include/libudev'))
-    extra_conf.append('DRM_COMPOSITOR_LIBS=-L' + ops.path_join(iopc.getSdkPath(), 'lib') + ' -ldrm -ludev')
-    extra_conf.append('LIBINPUT_BACKEND_CFLAGS=-I' + ops.path_join(iopc.getSdkPath(), 'usr/include/libinput'))
-    extra_conf.append('LIBINPUT_BACKEND_LIBS=-L' + ops.path_join(iopc.getSdkPath(), 'lib') + ' -linput')
-    extra_conf.append('COMPOSITOR_CFLAGS=-I' + ops.path_join(iopc.getSdkPath(), 'usr/include/wayland'))
-    extra_conf.append('COMPOSITOR_LIBS=-L' + ops.path_join(iopc.getSdkPath(), 'lib') + ' -lwayland-server')
-    extra_conf.append('DRM_COMPOSITOR_GBM_CFLAGS=-I' + ops.path_join(iopc.getSdkPath(), 'usr/include/mesa'))
-    extra_conf.append('DRM_COMPOSITOR_GBM_LIBS=-L' + ops.path_join(iopc.getSdkPath(), 'lib') + ' -lgbm')
+    cflags = ""
+    cflags += " -I" + ops.path_join(iopc.getSdkPath(), 'usr/include/libdrm')
+    cflags += " -I" + ops.path_join(iopc.getSdkPath(), 'usr/include/libdrm/libdrm')
+    cflags += " -I" + ops.path_join(iopc.getSdkPath(), 'usr/include/libudev')
+    cflags += " -I" + ops.path_join(iopc.getSdkPath(), 'usr/include/libinput')
+    cflags += " -I" + ops.path_join(iopc.getSdkPath(), 'usr/include/wayland')
+    cflags += " -I" + ops.path_join(iopc.getSdkPath(), 'usr/include/mesa')
+    cflags += " -I" + ops.path_join(iopc.getSdkPath(), 'usr/include/libpixman')
+    cflags += " -I" + ops.path_join(iopc.getSdkPath(), 'usr/include/libpng')
+    cflags += " -I" + ops.path_join(iopc.getSdkPath(), 'usr/include/libcairo')
+    cflags += " -I" + ops.path_join(iopc.getSdkPath(), 'usr/include/libxkbcommon')
+    cflags += " -I" + ops.path_join(iopc.getSdkPath(), 'usr/include/mesa/EGL')
+    cflags += " -I" + ops.path_join(iopc.getSdkPath(), 'usr/include/fontconfig')
+    cflags += " -I" + ops.path_join(iopc.getSdkPath(), 'usr/include/fontconfig/fontconfig')
+    cflags += " -I" + ops.path_join(iopc.getSdkPath(), 'usr/include/freetype')
+    cflags += " -I" + ops.path_join(iopc.getSdkPath(), 'usr/include/freetype/freetype2')
+    cflags += " -DMESA_EGL_NO_X11_HEADERS"
+
+    libs = ""
+    libs += " -L" + ops.path_join(iopc.getSdkPath(), 'lib')
+    libs += " -ldrm -ludev -linput -levdev -lwayland-server -lgbm -lwayland-client -lwayland-cursor -lwayland-egl -lpixman-1 -lffi -lpcre"
+    libs += " -lpng -lcairo -lxkbcommon -lEGL -lfreetype -lfontconfig -lGLESv2 -lOSMesa -lexpat -lxml2 -ludev -lmtdev -lz -lglapi -luuid -lglib-2.0"
+    extra_conf.append('LIBDRM_CFLAGS=' + cflags)
+    extra_conf.append('LIBDRM_LIBS=' + libs)
+    extra_conf.append('DRM_COMPOSITOR_CFLAGS=' + cflags)
+    extra_conf.append('DRM_COMPOSITOR_LIBS=' + libs)
+    extra_conf.append('LIBINPUT_BACKEND_CFLAGS=' + cflags)
+    extra_conf.append('LIBINPUT_BACKEND_LIBS=' + libs)
+    extra_conf.append('COMPOSITOR_CFLAGS=' + cflags)
+    extra_conf.append('COMPOSITOR_LIBS=' + libs)
+    extra_conf.append('DRM_COMPOSITOR_GBM_CFLAGS=' + cflags)
+    extra_conf.append('DRM_COMPOSITOR_GBM_LIBS=' + libs)
     #extra_conf.append('WAYLAND_PROTOCOLS_CFLAGS="-I' + ops.path_join(iopc.getSdkPath(), 'usr/include/wayland') + '"')
     #extra_conf.append('WAYLAND_PROTOCOLS_LIBS="-L' + ops.path_join(iopc.getSdkPath(), 'lib') + '"')
-    extra_conf.append('WAYLAND_COMPOSITOR_CFLAGS=-I' + ops.path_join(iopc.getSdkPath(), 'usr/include/wayland'))
-    extra_conf.append('WAYLAND_COMPOSITOR_LIBS=-L' + ops.path_join(iopc.getSdkPath(), 'lib') + ' -lwayland-client -lwayland-cursor')
-    extra_conf.append('PIXMAN_CFLAGS=-I' + ops.path_join(iopc.getSdkPath(), 'usr/include/libpixman'))
-    extra_conf.append('PIXMAN_LIBS=-L' + ops.path_join(iopc.getSdkPath(), 'lib') + ' -lpixman-1')
-    extra_conf.append('PNG_CFLAGS=-I' + ops.path_join(iopc.getSdkPath(), 'usr/include/libpng'))
-    extra_conf.append('PNG_LIBS=-L' + ops.path_join(iopc.getSdkPath(), 'lib') + ' -lpng')
-    extra_conf.append('CAIRO_CFLAGS=-I' + ops.path_join(iopc.getSdkPath(), 'usr/include/libcairo'))
-    extra_conf.append('CAIRO_LIBS=-L' + ops.path_join(iopc.getSdkPath(), 'lib') + ' -lcairo')
-    extra_conf.append('TEST_CLIENT_CFLAGS=-I' + ops.path_join(iopc.getSdkPath(), 'usr/include/wayland'))
-    extra_conf.append('TEST_CLIENT_LIBS=-L' + ops.path_join(iopc.getSdkPath(), 'lib') + ' -lwayland-client -lwayland-cursor')
-    extra_conf.append('CLIENT_CFLAGS=-I' + ops.path_join(iopc.getSdkPath(), 'usr/include/wayland'))
-    extra_conf.append('CLIENT_LIBS=-L' + ops.path_join(iopc.getSdkPath(), 'lib') + ' -lwayland-client -lwayland-cursor')
-    extra_conf.append('SERVER_CFLAGS=-I' + ops.path_join(iopc.getSdkPath(), 'usr/include/wayland'))
-    extra_conf.append('SERVER_LIBS=-L' + ops.path_join(iopc.getSdkPath(), 'lib') + ' -lwayland-server')
-    extra_conf.append('WESTON_INFO_CFLAGS=-I' + ops.path_join(iopc.getSdkPath(), 'usr/include/wayland'))
-    extra_conf.append('WESTON_INFO_LIBS=-L' + ops.path_join(iopc.getSdkPath(), 'lib') + ' -lwayland-client')
-    extra_conf.append('XKBCOMMON_COMPOSE_CFLAGS=-I' + ops.path_join(iopc.getSdkPath(), 'usr/include/libxkbcommon'))
-    extra_conf.append('XKBCOMMON_COMPOSE_LIBS=-L' + ops.path_join(iopc.getSdkPath(), 'lib') + ' -lxkbcommon')
-    extra_conf.append('EGL_CFLAGS=-I' + ops.path_join(iopc.getSdkPath(), 'usr/include/mesa/EGL') + ' -DMESA_EGL_NO_X11_HEADERS')
-    extra_conf.append('EGL_LIBS=-L' + ops.path_join(iopc.getSdkPath(), 'lib') + ' -lEGL')
-    extra_conf.append('EGL_TESTS_CFLAGS=-I' + ops.path_join(iopc.getSdkPath(), 'usr/include/mesa/EGL') + ' -DMESA_EGL_NO_X11_HEADERS')
-    extra_conf.append('EGL_TESTS_LIBS=-L' + ops.path_join(iopc.getSdkPath(), 'lib') + ' -lEGL')
-    extra_conf.append('WAYLAND_COMPOSITOR_EGL_CFLAGS=-I' + ops.path_join(iopc.getSdkPath(), 'usr/include/wayland') + ' -DMESA_EGL_NO_X11_HEADERS')
-    extra_conf.append('WAYLAND_COMPOSITOR_EGL_LIBS=-L' + ops.path_join(iopc.getSdkPath(), 'lib') + ' -lwayland-egl')
+    extra_conf.append('WAYLAND_COMPOSITOR_CFLAGS=' + cflags)
+    extra_conf.append('WAYLAND_COMPOSITOR_LIBS=' + libs)
+    extra_conf.append('PIXMAN_CFLAGS=' + cflags)
+    extra_conf.append('PIXMAN_LIBS=' + libs)
+    extra_conf.append('PNG_CFLAGS=' + cflags)
+    extra_conf.append('PNG_LIBS=' + libs)
+    extra_conf.append('CAIRO_CFLAGS=' + cflags)
+    extra_conf.append('CAIRO_LIBS=' + libs)
+    extra_conf.append('TEST_CLIENT_CFLAGS=' + cflags)
+    extra_conf.append('TEST_CLIENT_LIBS=' + libs)
+    extra_conf.append('CLIENT_CFLAGS=' + cflags)
+    extra_conf.append('CLIENT_LIBS=' + libs)
+    extra_conf.append('SERVER_CFLAGS=' + cflags)
+    extra_conf.append('SERVER_LIBS=' + libs)
+    extra_conf.append('WESTON_INFO_CFLAGS=' + cflags)
+    extra_conf.append('WESTON_INFO_LIBS=' + libs)
+    extra_conf.append('XKBCOMMON_COMPOSE_CFLAGS=' + cflags)
+    extra_conf.append('XKBCOMMON_COMPOSE_LIBS=' + libs)
+    extra_conf.append('EGL_CFLAGS=' + cflags)
+    extra_conf.append('EGL_LIBS=' + libs)
+    extra_conf.append('EGL_TESTS_CFLAGS=' + cflags)
+    extra_conf.append('EGL_TESTS_LIBS=' + libs)
+    extra_conf.append('WAYLAND_COMPOSITOR_EGL_CFLAGS=' + cflags)
+    extra_conf.append('WAYLAND_COMPOSITOR_EGL_LIBS=' + libs)
+    extra_conf.append('SIMPLE_EGL_CLIENT_CFLAGS=' + cflags)
+    extra_conf.append('SIMPLE_EGL_CLIENT_LIBS=' + libs)
     iopc.configure(tarball_dir, extra_conf)
 
     return True
@@ -191,11 +220,30 @@ def MAIN_BUILD(args):
     ops.mkdir(dst_usr_local_share_dir)
     ops.mkdir(dst_pkgconfig_dir)
 
+
     ops.copyto(ops.path_join(install_tmp_dir, "usr/local/bin/weston"), dst_bin_dir)
     ops.copyto(ops.path_join(install_tmp_dir, "usr/local/bin/weston-launch"), dst_bin_dir)
     ops.copyto(ops.path_join(install_tmp_dir, "usr/local/bin/weston-terminal"), dst_bin_dir)
     ops.copyto(ops.path_join(install_tmp_dir, "usr/local/bin/weston-info"), dst_bin_dir)
     ops.copyto(ops.path_join(output_dir, "weston_init.sh"), dst_bin_dir)
+
+    if install_test_utils :
+        ops.copyto(ops.path_join(install_tmp_dir, "usr/local/bin/weston-calibrator"), dst_bin_dir)
+        ops.copyto(ops.path_join(install_tmp_dir, "usr/local/bin/weston-clickdot"), dst_bin_dir)
+        ops.copyto(ops.path_join(install_tmp_dir, "usr/local/bin/weston-cliptest"), dst_bin_dir)
+        ops.copyto(ops.path_join(install_tmp_dir, "usr/local/bin/weston-confine"), dst_bin_dir)
+        ops.copyto(ops.path_join(install_tmp_dir, "usr/local/bin/weston-dnd"), dst_bin_dir)
+        ops.copyto(ops.path_join(install_tmp_dir, "usr/local/bin/weston-eventdemo"), dst_bin_dir)
+        ops.copyto(ops.path_join(install_tmp_dir, "usr/local/bin/weston-flower"), dst_bin_dir)
+        ops.copyto(ops.path_join(install_tmp_dir, "usr/local/bin/weston-fullscreen"), dst_bin_dir)
+        ops.copyto(ops.path_join(install_tmp_dir, "usr/local/bin/weston-image"), dst_bin_dir)
+        ops.copyto(ops.path_join(install_tmp_dir, "usr/local/bin/weston-resizor"), dst_bin_dir)
+        ops.copyto(ops.path_join(install_tmp_dir, "usr/local/bin/weston-scaler"), dst_bin_dir)
+        ops.copyto(ops.path_join(install_tmp_dir, "usr/local/bin/weston-simple-egl"), dst_bin_dir)
+        ops.copyto(ops.path_join(install_tmp_dir, "usr/local/bin/weston-smoke"), dst_bin_dir)
+        ops.copyto(ops.path_join(install_tmp_dir, "usr/local/bin/weston-stacking"), dst_bin_dir)
+        ops.copyto(ops.path_join(install_tmp_dir, "usr/local/bin/weston-subsurfaces"), dst_bin_dir)
+        ops.copyto(ops.path_join(install_tmp_dir, "usr/local/bin/weston-transformed"), dst_bin_dir)
 
     libweston = "libweston-4.so.0.0.0"
     ops.copyto(ops.path_join(install_tmp_dir, "usr/local/lib/" + libweston), dst_lib_dir)
@@ -218,15 +266,12 @@ def MAIN_BUILD(args):
     libweston_dir = ops.path_join(dst_usr_local_lib_dir, "weston")
     ops.mkdir(libweston_dir)
     ops.copyto(ops.path_join(install_tmp_dir, "usr/local/lib/weston/desktop-shell.so"), libweston_dir)
-    #ops.copyto(ops.path_join(install_tmp_dir, "usr/local/lib/weston/fullscreen-shell.so"), libweston_dir)
+    ops.copyto(ops.path_join(install_tmp_dir, "usr/local/lib/weston/fullscreen-shell.so"), libweston_dir)
 
     ops.mkdir(dst_usr_local_share_dir)
 
-    wayland_session = "wayland-sessions"
-    ops.copyto(ops.path_join(install_tmp_dir, "usr/local/share/" + wayland_session), dst_usr_local_share_dir)
-
-    weston = "weston"
-    ops.copyto(ops.path_join(install_tmp_dir, "usr/local/share/" + weston), dst_usr_local_share_dir)
+    ops.copyto(ops.path_join(install_tmp_dir, "usr/local/share/wayland-sessions"), dst_usr_local_share_dir)
+    ops.copyto(ops.path_join(install_tmp_dir, "usr/local/share/weston"), dst_usr_local_share_dir)
 
     ops.copyto(ops.path_join(install_tmp_dir, "usr/local/libexec/."), dst_usr_local_libexec_dir)
 
